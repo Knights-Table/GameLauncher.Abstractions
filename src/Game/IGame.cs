93f40bb635ba89154c0e2d075dcd8e43b0f64736
Austin Vandersluis <austinv900@nindroidservers.com>
2020-08-04 22:11:46 -0700

    Initial commit

---
+++ src/Game/IGame.cs
@@ -0,0 +1,51 @@
+﻿namespace GameLauncher.Game
+{
+    public interface IGame
+    {
+        /// <summary>
+        /// The name of the game
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// The AppId for the game
+        /// </summary>
+        uint AppId { get; }
+
+        /// <summary>
+        /// The local directory where game content is saved
+        /// </summary>
+        string Directory { get; }
+
+        /// <summary>
+        /// The banner image for the game
+        /// </summary>
+        string Banner { get; }
+
+        /// <summary>
+        /// The icon for the game
+        /// </summary>
+        string Icon { get; }
+
+        /// <summary>
+        /// Sets the directory where game files are stored
+        /// </summary>
+        /// <param name="directory">The directory</param>
+        /// <param name="moveExisting">If old directory exists and contains files move files to new directory</param>
+        /// <returns>A observable move operation</returns>
+        IObservableOperation SetLocalDirectory(string directory, bool moveExisting = false);
+
+        /// <summary>
+        /// Delete game files
+        /// </summary>
+        /// <param name="includeSaveData">Include user specific information</param>
+        /// <returns>Watchable operation</returns>
+        IObservableOperation DeleteGame(bool includeSaveData = false);
+
+        /// <summary>
+        /// Launch the game
+        /// </summary>
+        /// <param name="parameters">Parameters used to launch game</param>
+        void LaunchGame(string[] parameters);
+    }
+}
---
+++ src/IObservableOperation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GameLauncher
+{
+    public interface IObservableOperation
+    {
+        /// <summary>
+        /// The name of this operation
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Current Progress
+        /// </summary>
+        double Current { get; }
+
+        /// <summary>
+        /// Completed Value
+        /// </summary>
+        double Max { get; }
+
+        /// <summary>
+        /// True if this operation has completed
+        /// </summary>
+        bool Completed { get; }
+
+        /// <summary>
+        /// Subscribe to progress updates for this operation
+        /// </summary>
+        /// <param name="callback">The callback to invoke when progress has been changed</param>
+        void Subscribe(Action<IObservableOperation> callback);
+    }
+}
